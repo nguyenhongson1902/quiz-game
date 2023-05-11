4a83ae0a68b3d6e161bffd0311fbe3f5a2ee52ae
thaibahung <tbhung1001@gmail.com>
2023-05-11 17:22:51 +0700

    Second Code Update

--- quiz_game.py
+++ quiz_game.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 
 data = pd.read_csv("./quiz_questions.csv")
+data.rename(columns={'Option 1': 'A', 'Option 2': 'B', 'Option 3' : 'C', 'Option 4' : 'D'}, inplace=True)
 
 def load_questions():
     num_question = len(data.index)
@@ -14,11 +15,26 @@ def load_questions():
 def ask_question(question):
     print("Here is your question: ")
     print(question["Question"])
+    for i in range(0, 4) :
+        c = chr(ord("A") + i)
+        print(c + " " + question[c])
+    ans = input("Enter your answer: ")
+    if question[ans] == question['Answer'] :
+        print("Correct!!!!")
+        print("The answer is: " + question['Answer'])
+        return 1
+    else :
+        print("Wrong Answer")
+        print("The answer is: " + question['Answer'])
+        return 0
 
 def run_quiz():
     questions_list = load_questions()
-    ask_question(questions_list[0])
+    score = 0
+    for i in range(0, 5) :
+        score += ask_question(questions_list[i])
 
+    print(score)
 
 if __name__ == "__main__":
     run_quiz()
